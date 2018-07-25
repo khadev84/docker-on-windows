4fa1e0b2f3d32e18d8aa500e993cd8e396dc0aa1
VISHAL KHADE <khadev@ds.ad.adp.com>
2018-07-25 06:55:07 +0530

    docker images build files

---
+++ ch03/ch03-nerd-dinner-db/src/NerdDinner.Database/dbo/Tables/Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Fname] NCHAR(10) NULL, 
+    [Lname] NCHAR(10) NULL, 
+    [Address] NCHAR(10) NULL, 
+    [Gender] TEXT NULL
+)
